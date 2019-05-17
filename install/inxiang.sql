9b748b0ae0ad1ab4209b4b4859f9d3cbbb1d9bd4
RCWei <rcwei44@qq.com>
2019-05-17 09:28:57 +0800

    更新sql文件

--- install/inxiang.sql
+++ install/inxiang.sql
@@ -3,18 +3,18 @@
 
  Source Server         : LOCALHOST
  Source Server Type    : MySQL
- Source Server Version : 100136
+ Source Server Version : 100138
  Source Host           : localhost:3306
  Source Schema         : inxiang
 
  Target Server Type    : MySQL
- Target Server Version : 100136
+ Target Server Version : 100138
  File Encoding         : 65001
 
- Date: 06/04/2019 17:41:30
+ Date: 17/05/2019 09:26:38
 */
 
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -32,8 +32,8 @@ CREATE TABLE `in_friend`  (
 -- ----------------------------
 -- Records of in_friend
 -- ----------------------------
-INSERT INTO `in_friend` VALUES (1, 'rcwei28', 'rcwei', '2019-03-19 10:00:14');
-INSERT INTO `in_friend` VALUES (2, 'rcwei', 'rcwei28', '2019-03-26 10:20:37');
+INSERT INTO `in_friend` VALUES (1, 'rcwei28', 'Cellur', '2019-03-19 10:00:14');
+INSERT INTO `in_friend` VALUES (2, 'Cellur', 'rcwei28', '2019-03-26 10:20:37');
 
 -- ----------------------------
 -- Table structure for in_gallery
@@ -48,13 +48,12 @@ CREATE TABLE `in_gallery`  (
   `likes` bigint(10) NULL DEFAULT NULL COMMENT '点赞数',
   `desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`, `author_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of in_gallery
 -- ----------------------------
-INSERT INTO `in_gallery` VALUES (1, 1, 'rcwei28', 'http://inxiang.net/upload/gallery/5c8bcd1282b79.jpeg', '2019-03-16 00:04:34', NULL, 'little space😜😜');
-INSERT INTO `in_gallery` VALUES (2, 2, 'rcwei', 'http://inxiang.net/upload/gallery/5c8f44858564c.jpeg', '2019-03-18 15:11:01', NULL, '教练，我想学树莓派！！');
+INSERT INTO `in_gallery` VALUES (2, 2, 'Cellur', 'http://inxiang.net/upload/gallery/5c8f44858564c.jpeg', '2019-03-18 15:11:01', NULL, '教练，我想学树莓派！！');
 
 -- ----------------------------
 -- Table structure for in_user
@@ -65,7 +64,6 @@ CREATE TABLE `in_user`  (
   `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '昵称',
   `password` char(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '密码',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL COMMENT '电子邮箱',
-  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL COMMENT '头像',
   `saying` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL COMMENT '个性签名',
   `phone` bigint(11) NULL DEFAULT NULL COMMENT '电话号码',
   `gender` int(1) UNSIGNED NULL DEFAULT 0 COMMENT '性别',
@@ -76,7 +74,7 @@ CREATE TABLE `in_user`  (
 -- ----------------------------
 -- Records of in_user
 -- ----------------------------
-INSERT INTO `in_user` VALUES (1, 'rcwei28', '301f912a4b17af1c26d581a7557f4ed8e2e572ed', 'rcwei44@qq.com', 'http://inxiang.net/upload/avatar/5ca0a4af9bfbb.jpg', '退潮后的水沫幻影', 13610460382, 0, 1);
-INSERT INTO `in_user` VALUES (2, 'rcwei', 'd8db4b185013896534bf97f8972681b05028e742', 'rcwei28@sina.com', 'http://inxiang.net/upload/avatar/5c8f434d20035.jpg', 'Think defferent', 12345678901, 0, 1);
+INSERT INTO `in_user` VALUES (1, 'rcwei28', '301f912a4b17af1c26d581a7557f4ed8e2e572ed', 'rcwei44@qq.com', '退潮后的水沫幻影', 13610460382, 0, 1);
+INSERT INTO `in_user` VALUES (2, 'Cellur', '5831122c93f7a17fb7453cfe5684eb8809462d32', 'rcwei28@sina.com', 'Think defferent', 12345678901, 0, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
