8bdbe6cccb365975811bef85b54376a079e27895
RCWei <rcwei44@qq.com>
2019-05-22 10:46:37 +0800

    图片删除

--- application/index/controller/Detail.php
+++ application/index/controller/Detail.php
@@ -25,66 +25,25 @@ class Detail extends Controller
             $path = G::where('id', $id)->value('path');
             if (strtoupper(substr(PHP_OS,0,3))==='WIN') {
                 // in windows
-                $path = str_replace('/', '\\', str_replace('http://inxiang.net/', ROOT_PATH.DS.'public'.DS, $path));
+                echo $path = str_replace('/', '\\', str_replace('http://inxiang.net/', ROOT_PATH.'public'.DS, $path));
                 if (db('gallery')->where('id', $id)->delete()) {
                     unlink($path);
-                    echo "<script>alert('删除成功');window.opener=null;
+                    echo "<script>alert('删除成功');window.opener.location.href = window.opener.location.href;
                     window.open('','_self');window.close();</script>";
-                    return $this->redirect('http://inxiang.net/user');
                 } else {
                     echo "<script>alert('删除失败'); history.back();</script>";
                 }
             } else {
                 // in linux
                 if (db('gallery')->where('id', $id)->delete()) {
-                    unlink(str_replace('http://inxiang.net/', ROOT_PATH.DS.'public'.DS, $path));
-                    echo "<script>alert('删除成功');window.opener=null;
+                    unlink(str_replace('http://inxiang.net/', ROOT_PATH.'public'.DS, $path));
+                    echo "<script>alert('删除成功');window.opener.location.href = window.opener.location.href;
                     window.open('','_self');window.close();</script>";
-                    return $this->redirect('http://inxiang.net/user');
                 } else {
                     echo "<script>alert('删除失败'); history.back();</script>";
                 }
             }
         } else { echo '<script>alert("删除操作仅限作者使用");history.go(-1)</script>'; }
-    }
-
-    public function Down() {
-        header('Content-Type:application/octet-stream;');
-        $id = input('id');
-        $path = G::where('id', $id)->value('path');
-        if (strtoupper(substr(PHP_OS,0,3))==='WIN') {
-            // in windows
-            $path = str_replace('/', '\\', str_replace('http://inxiang.net/', ROOT_PATH.DS.'public'.DS, $path));
-            $name = uniqid().rand(1,1024).'.jpg';
-            $fs = filesize($path);
-            $fp = fopen($path, 'r');
-            header("Content-Disposition:attachment; filename=$name");
-            $buffer = 1024;
-            $i = 0;
-
-            while (!feof($fp) && ($fs - $i > 0)) {
-                $data = fread($fp, $buffer);
-                $i += $buffer;
-                echo $data;
-            }
-            fclose($fp);
-        } else {
-            // in linux
-            $path = str_replace('http://inxiang.net/', ROOT_PATH.DS.'public'.DS, $path);
-            $name = uniqid().rand(1,1024).'.jpg';
-            $fs = filesize($path);
-            $fp = fopen($path, 'r');
-            header("Content-Disposition:attachment; filename=$name");
-            $buffer = 1024;
-            $i = 0;
-
-            while (!feof($fp) && ($fs - $i > 0)) {
-                $data = fread($fp, $buffer);
-                $i += $buffer;
-                echo $data;
-            }
-            fclose($fp);
-        }
     }
 
     public function Friend() {
