79ab010c404f17b338fc5979dd70246c78e44e7f
RCWei <rcwei44@qq.com>
2019-05-22 10:26:01 +0800

    0522 修改

--- application/index/controller/Like.php
+++ application/index/controller/Like.php
@@ -11,4 +11,11 @@ class Like extends Controller
         return '<script>window.location.href="http://inxiang.net";</script>';
     }
 }
+
+//TODO: 判断是否重复点赞
+// if (in_like 表中有 cookie('name') 的行) {
+//     提醒不能重复点赞
+// } else {
+//     向表中添加一行
+// }
 ?>
