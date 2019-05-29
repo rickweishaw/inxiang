54ad730ab359495c9db10acd44d0fa8f7f190715
RCWei <rcwei44@qq.com>
2019-05-29 10:21:31 +0800

    搜索完成

--- application/search/controller/Index.php
+++ application/search/controller/Index.php
@@ -4,9 +4,12 @@ use think\Controller;
 use think\Request;
 use app\search\model\User as U;
 
-class Index extends Controller
-{
-    public function Index(){
+class Index extends Controller {
+    public function Index() {
+        $string = input('get.keyword');
+        $result = U::where('nickname', 'LIKE', '%'.$string.'%')->select();
+        $this -> assign('count', count($result));
+        $this -> assign('result', $result);
         return view('search');
     }
 }
